e2a6d6f30d2c82649daead4209750134634aefcc
RonaldKarpovv <95498992+RonaldKarpovv@users.noreply.github.com>
2023-06-01 13:13:06 +0200

    Create lab2.py

---
+++ lab2.py
@@ -0,0 +1,34 @@
+import queue
+def bfs(graph, start):
+    # создаем очередь для обхода вершин в ширину
+    q = queue.Queue()
+    # добавляем начальную вершину в очередь
+    q.put(start)
+    # создаем словарь для хранения расстояний от начальной вершины
+    # до остальных вершин графа
+    distances = {start: 0}
+    # выполняем обход в ширину
+    while not q.empty():
+        # извлекаем вершину из очереди
+        current = q.get()
+        # проходим по всем соседним вершинам
+        for neighbor in range(len(graph[current])):
+            # если между текущей и соседней вершинами есть ребро
+            if graph[current][neighbor] != 0:
+                # если соседняя вершина еще не была посещена
+                if neighbor not in distances:
+                    # добавляем ее в очередь и вычисляем расстояние
+                    q.put(neighbor)
+                    distances[neighbor] = distances[current] + 1
+    # возвращаем словарь расстояний
+    return distances
+# читаем матрицу смежности из файла
+with open('graph.txt') as f:
+    lines = f.readlines()
+    # преобразуем строки в числа
+    matrix = [[int(x) for x in line.split()] for line in lines]
+# вызываем функцию поиска кратчайшего пути
+distances = bfs(matrix, 0)
+# выводим расстояния до всех вершин графа
+for vertex, distance in distances.items():
+    print('Расстояние до вершины {}: {}'.format(vertex, distance))
